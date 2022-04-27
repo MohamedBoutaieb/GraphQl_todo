6127d64d82aaff17e6caf37afc44d28a419158db
Mohamed Boutaieb <mohamed.boutaieb.kaoun@gmail.com>
2022-04-27 15:45:50 +0100

    todo subscription

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer ,PubSub} from 'graphql-yoga'
 import { Query } from './resolvers/Query.js'
 import { Todo } from './resolvers/Todo.js';
 import { User } from './resolvers/User.js';
-
+import { Mutation } from './resolvers/Mutation.js';
+import {db} from './data/db.js';
+import { Subscription } from "./resolvers/Subscription.js";
 // ... or using "require()"
 // const { GraphQLServer } = require('graphql-yoga')
+
 const typeDefs = "schema/schema.graphql";
+const pubsub = new PubSub();
 const resolvers = {
-    Query,Todo, User};
-const server = new GraphQLServer({ typeDefs, resolvers })
+    Query,Todo, 
+    Subscription,User,Mutation};
+const server = new GraphQLServer({ typeDefs, resolvers,context:{  db,pubsub} })
 server.start(() => console.log('Server is running on localhost:4000'))
---
+++ resolvers/Mutation.js
@@ -0,0 +1,52 @@
+
+import {v4 as uuidv4} from 'uuid';
+
+export const Mutation = {
+    addTodo: (parent, { addTodoInput }, { db, pubsub }, infos) => {
+        
+        const user = db.users.find((user) => user.id ===addTodoInput.userId);
+        if (!user) {
+          throw new Error(`Le user d'id ${addTodoInput.userId} n'existe pas`);
+        } else {
+          const newTodo = {
+            id: db.todos.length +1,
+            status: "WAITING",
+            ...addTodoInput,
+          };
+          db.todos.push(newTodo);
+          pubsub.publish("todo", { todo: { todo: newTodo, mutation: "ADD" } });
+          return newTodo;
+        }
+     
+      },
+      updateTodo: (parent, { id, updateTodoInput }, { db, pubsub }, infos) => {
+        const user = db.users.find((user) => user.id ===addTodoInput.userId);
+        if (
+          !user
+        ) {
+          throw new Error(`Le user d'id ${updateTodoInput.userId} n'existe pas`);
+        } else {
+          //Il faut que l'id du todo existe
+          const todo = db.todos.find((todoItem) => todoItem.id === id);
+          if (!todo) {
+            throw new Error(`Le todo d'id ${id} n'existe pas`);
+          } else {
+            for (let key in updateTodoInput) {
+              todo[key] = updateTodoInput[key];
+            }
+            pubsub.publish("todo", { todo: { todo, mutation: "UPDATE" } });
+            return todo;
+          }
+        }
+      },
+      deleteTodo: (parent, { id }, { db, pubsub }, infos) => {
+        const todo = db.todos.find(id);
+        if (!todo) {
+          throw new Error("Todo innexistant");
+        } else {
+          const newTodo = db.todo.filter((todo)=>(todo.id!= id));
+          pubsub.publish("todo", { todo: { newTodo, mutation: "DELETE" } });
+          return newTodo;
+        }
+      },
+}
